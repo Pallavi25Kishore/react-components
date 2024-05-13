b69cf972cb3e3c9efd506cc33f75f90b164a7298
Pallavi <pallavi25kishore@gmail.com>
2024-05-13 10:34:54 -0700

    create and render a React component

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-// TODO
+//creating and rendering a React component
+const GroceryList = () => <ul><li>item1</li><li>item2</li></ul>;
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
