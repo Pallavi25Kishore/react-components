34d7266c9811dd13129c0c65d554584b590bcc68
Pallavi <pallavi25kishore@gmail.com>
2024-05-13 11:18:17 -0700

    create components with props

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//creating and rendering a React component
+/*//creating and rendering a React component
 const GroceryList = () => <ul><li>Cucumbers</li><li>Kale</li></ul>;
 ReactDOM.render(<GroceryList />, document.getElementById('app'));
 
@@ -9,7 +9,21 @@ const GroceryList = () => (<ul>
   <Cucumbers />
   <Kale />
 </ul>);
+ReactDOM.render(<GroceryList />, document.getElementById('app'));*/
+
+//component properties aka props
+const GroceryListItem = (props) => (
+  <ul>
+<li>{props.item[0]}</li>
+<li>{props.item[1]}</li>
+</ul>
+);
+const GroceryList = () => (<div>
+  <GroceryListItem item={['Cucumbers', 'Kale']} />
+</div>);
 ReactDOM.render(<GroceryList />, document.getElementById('app'));
+
+
 
 
 
